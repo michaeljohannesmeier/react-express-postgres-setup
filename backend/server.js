81ba70426137224ad625d4736bdbac789547740f
Michael Meier <michael_johannes.meier@daimler.com>
2022-01-04 22:45:17 +0100

    http tests, no logs pgadmin

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
+// imports
 const express = require("express");
 const bodyParser = require('body-parser')
 const app = express();
+const { Pool } = require('pg')
+
+// middlewares
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
-const port = 3001;
 
-const { Pool } = require('pg')
+// connection to database
 const pool = new Pool({
   user: "postgres",
   host: "test-postgres",
@@ -17,7 +20,13 @@ const pool = new Pool({
   password: "test-postgres",
   port: "5432"
 });
-console.log('connected..')
+
+
+// create tables if they do not already exist
+pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), email VARCHAR(200));')
+
+
+// database queries
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users', (error, results) => {
     if (error) {
@@ -26,12 +35,18 @@ const getUsers = (request, response) => {
     response.status(200).json(results.rows)
   })
 }
+
+
+// rest api endpoints
 app.get("/", async (req, res) => {
   console.log("in / get request");
   res.send("Hello World!");
 });
 app.get("/users", getUsers);
 
+
+// server start
+const port = 3001;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
